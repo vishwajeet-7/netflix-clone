510ee16fc594acd425296d8124f7d71a004143af
vishwajeet-7 <vishady7@gmail.com>
2023-04-26 02:09:57 +0530

    started navbar

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HomeScreen.css';
+import Navbar from './Navbar';
+
+const HomeScreen = () => {
+  return (
+    <div className='homescreen'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='nav nav__black'>
+        <div className="nav__contents">
+        <img className='nav__logo' src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="netflix-logo" />
+        <img className='nav__avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="avatar" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
