d3e14c91e21050392bc5380d4d3544bcee188b08
vishwajeet-7 <vishady7@gmail.com>
2023-04-27 00:44:56 +0530

    working on home page

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -6,6 +6,31 @@ const HomeScreen = () => {
   return (
     <div className='homescreen'>
       <Navbar/>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      
+      {/* {Banner} */}
     </div>
   )
 }
