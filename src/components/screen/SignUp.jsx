ecb231c8dc4653a287ae5fd39a2bd6a5052bc7cb
vishwajeet-7 <vishady7@gmail.com>
2023-04-30 00:42:48 +0530

    made login and signup components

--- src/components/screen/SignUp.jsx
+++ src/components/screen/SignUp.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import './SignUp.css'
+import React from "react";
+import "./SignUp.css";
 
 const SignUp = () => {
+  const register = (e)=>{
+    e.preventDefault();
+  }
+
+  const signIn = (e)=>{
+    e.preventDefault();
+  }
   return (
-    <div className='signup'>
+    <div className="signup">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder='Email' />
-        <input type="password" placeholder='Password' />
-        <button type='submit'>Sign In</button>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit" onClick={signIn}>Sign In</button>
+        <h4>
+          <span className="signup_gray">New to Netflix? </span> <span className="signup_link" onClick={register}>Sign up now.</span>
+        </h4>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
